f346204449d0bb8f62b67ce8432ada91710c8d56
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-03-02 03:23:48 +0500

    2-masala

--- main.js
+++ main.js
@@ -7,3 +7,35 @@
 // } else {
 //     alert(number - 2)
 // }
+
+// 2-Masala;
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//     musbat++
+// } 
+// if(a < 0){
+//     manfiy++
+// }
+// if(b > 0){
+//     musbat++
+// }
+// if(b < 0 ){
+//     manfiy++
+// }
+// if(c > 0){
+//     musbat++
+// }
+// if(c < 0){
+//     manfiy++
+// }
+
+// console.log("Musbat sonlar soni", musbat, "Manfiy sonlar soni", manfiy);
+
+
